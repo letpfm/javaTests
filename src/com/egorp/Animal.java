ec6cde5f540c538afa18db09c2b163b0df1082a0
Zyuzin Oleg <olzyuzin@gmail.com>
2018-02-03 20:30:03 +0400

    Inital structure

---
+++ src/com/egorp/Animal.java
@@ -0,0 +1,9 @@
+package com.egorp;
+
+public class Animal {
+    private int age;
+
+    functuon getAge() {
+        retru
+    }
+}
---
+++ src/com/egorp/Main.java
@@ -0,0 +1,9 @@
+package com.egorp;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal lion = new Animal()
+       System.out.println("Kokokoko");
+    }
+
+}
